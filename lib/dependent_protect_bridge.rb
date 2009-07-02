30555e8c408089434fbeca0764e82482ccf8ce64
Sergio <sergio@entrecables.com>
2009-07-02 12:54:22 +0200

    add tests

--- lib/dependent_protect_bridge.rb
+++ lib/dependent_protect_bridge.rb
@@ -1,4 +1,5 @@
 module DependentProtectSecurity
+  protected
   def authorized_for_destroy?
     self.class.reflect_on_all_associations.all? do |association|
       association.options[:dependent] != :protect || self.send(association.name).blank?
