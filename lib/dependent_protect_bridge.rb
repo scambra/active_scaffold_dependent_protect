6c453c7deb81c53e08e568b51bbd20cd84012652
scambra <sergio@entrecables.com>
2010-03-01 10:02:11 +0100

    rename to authorized_for_destroy? to remove ActiveScaffold deprecation

--- lib/dependent_protect_bridge.rb
+++ lib/dependent_protect_bridge.rb
@@ -1,6 +1,6 @@
 module DependentProtectSecurity
   protected
-  def authorized_for_destroy?
+  def authorized_for_delete?
     self.class.reflect_on_all_associations.all? do |association|
       association.options[:dependent] != :protect || self.send(association.name).blank?
     end
